056f0795cfdc679d6c00438affd841cb1f157c23
Artem Kostuchenko <ipz204_kav@student.ztu.edu.ua>
2023-11-20 16:57:54 +0200

    Created PointsAverage project

--- Sana02/PointsAverage/Program.cs
+++ Sana02/PointsAverage/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// sum of points
+int sumPoints = 0;
+
+// number of points
+int numPoints = 5; 
+
+int index = 0;
+
+while(index < numPoints)
+{
+    int point;
+
+    Console.Write("Enter {0} point: ", index + 1);
+
+    while (true)
+    {
+        bool valid = int.TryParse(Console.ReadLine(), out point);
+
+        if (valid)
+            break;
+
+        Console.Write("Again enter {0} point: ", index + 1);
+    }
+
+    sumPoints += point;
+    index++;
+}
+
+double avg = sumPoints / numPoints;
+
+if (avg >= 50)
+{
+    Console.WriteLine("Student's assumptions for the exam -> {0} > 50", avg);
+}
+else
+{
+    Console.WriteLine("Student not admitted to the exam -> {0} < 50", avg);
+}
