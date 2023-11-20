b2accde4e97f59be0f5898505f94e67249d4893b
Artem Kostuchenko <ipz204_kav@student.ztu.edu.ua>
2023-11-20 17:41:25 +0200

    Created ExpressionsLibrary and While 1 project

---
+++ Sana02/While1/Program.cs
@@ -0,0 +1,23 @@
+﻿using ExpressionsLibrary;
+
+int n;
+
+while (true)
+{
+    Console.Write("Enter n:");
+
+    bool valid = int.TryParse(Console.ReadLine(), out n);
+
+    if (valid)
+        break;
+
+    Console.WriteLine("Please, enter a valid integer number");
+}
+
+double a = Expressions.ArcTgSeries(n);
+
+double b = Expressions.BaselSeries(n);
+
+double c = Expressions.SumOfFactorials(n);
+
+Console.WriteLine("\nResults:\na = {0}\nb={1}\nc={2}", a, b, c);
